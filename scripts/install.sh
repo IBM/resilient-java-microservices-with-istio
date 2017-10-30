340997c9f02555aa16c02b99950ec5e05ff313ab
matthew wagoner <zxkuqyb@gmail.com>
2017-10-30 11:51:01 -0400

    Fix Bluemix auth
    Previously, Travis runs were not working with the message
    "Credentials were rejected, please try again."  This commit updates
    credentials and sets up new scripts for authenticating with bluemix
    and installing kubectl

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,120 +1,34 @@
 #!/bin/bash
 
-function install_bluemix_cli() {
-#statements
-echo "Installing Bluemix cli"
-curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
-sudo mv cf /usr/local/bin
-sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
-cf --version
-curl -L public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_0.5.4_amd64.tar.gz > Bluemix_CLI.tar.gz
+# This script is intended to be run by Travis CI. If running elsewhere, invoke
+# it with: TRAVIS_PULL_REQUEST=false [path to script]
+
+# shellcheck disable=SC1090
+source "$(dirname "$0")"/../scripts/resources.sh
+
+is_pull_request "$0"
+
+echo "Install Bluemix CLI"
+curl -L https://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/latest/Bluemix_CLI_amd64.tar.gz > Bluemix_CLI.tar.gz
 tar -xvf Bluemix_CLI.tar.gz
 sudo ./Bluemix_CLI/install_bluemix_cli
-}
 
-function bluemix_auth() {
-echo "Authenticating with Bluemix"
-echo "1" | bx login -a https://api.ng.bluemix.net -u $BLUEMIX_USER -p $BLUEMIX_PASS
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-bx plugin install container-service -r Bluemix
-echo "Installing kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-}
+echo "Install kubectl"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/"$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"/bin/linux/amd64/kubectl
+chmod 0755 kubectl
+sudo mv kubectl /usr/local/bin
 
-function cluster_setup() {
-bx cs workers $CLUSTER
-$(bx cs cluster-config $CLUSTER | grep export)
-
-#Delete previous deployment and change image names.
-kubectl delete --ignore-not-found=true -f manifests/deploy-schedule.yaml
-kubectl delete --ignore-not-found=true -f manifests/deploy-session.yaml
-kubectl delete --ignore-not-found=true -f manifests/deploy-speaker.yaml
-kubectl delete --ignore-not-found=true -f manifests/deploy-vote.yaml
-kubectl delete --ignore-not-found=true -f manifests/deploy-webapp.yaml
-kubectl delete --ignore-not-found=true -f manifests/deploy-cloudant.yaml
-kubectl delete --ignore-not-found=true -f manifests/deploy-job.yaml
-kubectl delete --ignore-not-found=true -f manifests/ingress.yaml
-
+echo "install Istio"
 curl -L https://git.io/getIstio | sh -
 cd $(ls | grep istio)
-sudo mv bin/istioctl /usr/local/bin/
-
+export PATH="$PATH:$(pwd)/bin"
 kubectl apply -f install/kubernetes/istio-rbac-alpha.yaml
 kubectl apply -f install/kubernetes/istio.yaml
 
-PODS=$(kubectl get pods | grep istio | grep Pending)
-while [ ${#PODS} -ne 0 ]
-do
-    echo "Some Pods are Pending..."
-    PODS=$(kubectl get pods | grep istio | grep Pending)
-    sleep 5s
-done
+echo "Install the Bluemix container-service plugin"
+bx plugin install container-service -r Bluemix
 
-PODS=$(kubectl get pods | grep istio | grep ContainerCreating)
-while [ ${#PODS} -ne 0 ]
-do
-    echo "Some Pods are still creating Containers..."
-    PODS=$(kubectl get pods | grep istio | grep ContainerCreating)
-    sleep 5s
-done
-}
-
-function initial_setup() {
-echo "Creating Java MicroProfile with Injected Envoys..."
-cd ..
-kubectl create -f manifests/ingress.yaml
-kubectl apply -f manifests/deploy-job.yaml
-kubectl apply -f <(istioctl kube-inject -f manifests/deploy-schedule.yaml)
-kubectl apply -f <(istioctl kube-inject -f manifests/deploy-session.yaml)
-kubectl apply -f <(istioctl kube-inject -f manifests/deploy-speaker.yaml)
-kubectl apply -f <(istioctl kube-inject -f manifests/deploy-cloudant.yaml --includeIPRanges=172.30.0.0/16,172.20.0.0/16)
-kubectl apply -f <(istioctl kube-inject -f manifests/deploy-vote.yaml)
-kubectl apply -f <(istioctl kube-inject -f manifests/deploy-webapp.yaml)
-
-PODS=$(kubectl get pods | grep Init)
-while [ ${#PODS} -ne 0 ]
-do
-    echo "Some Pods are Initializing..."
-    PODS=$(kubectl get pods | grep Init)
-    sleep 5s
-done
-
-echo "MicroProfile done."
-
-}
-
-function health_check() {
-
-export GATEWAY_URL=$(bx cs workers $CLUSTER | grep normal | awk '{ print $2 }' | head -1):$(kubectl get svc istio-ingress -o jsonpath={.spec.ports[0].nodePort})
-sleep 150s #wait for Websphere Liberty to be up
-export HEALTH=$(curl -o /dev/null -s -w "%{http_code}\n" http://$GATEWAY_URL)
-if [ $HEALTH -eq 200 ]
-then
-  echo "Everything looks good."
-  echo "Cleaning up."
-  kubectl delete --ignore-not-found=true -f manifests/deploy-schedule.yaml
-  kubectl delete --ignore-not-found=true -f manifests/deploy-session.yaml
-  kubectl delete --ignore-not-found=true -f manifests/deploy-speaker.yaml
-  kubectl delete --ignore-not-found=true -f manifests/deploy-vote.yaml
-  kubectl delete --ignore-not-found=true -f manifests/deploy-webapp.yaml
-  kubectl delete --ignore-not-found=true -f manifests/deploy-cloudant.yaml
-  kubectl delete --ignore-not-found=true -f manifests/deploy-job.yaml
-  kubectl delete --ignore-not-found=true -f manifests/ingress.yaml
-  cd $(ls | grep istio)
-  kubectl delete -f install/kubernetes/istio.yaml
-  kubectl delete -f install/kubernetes/istio-rbac-alpha.yaml
-  echo "Deleted Istio in cluster"
-else
-  echo "Health check failed."
-  exit 1
+if [ -n "$DEBUG" ]; then
+  bx --version
+  bx plugin list
 fi
-}
-
-
-
-install_bluemix_cli
-bluemix_auth
-cluster_setup
-initial_setup
-health_check
